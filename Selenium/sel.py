d11c066e32c90ba31c0e7edabef3042622692dda
SuryaSekhar14 <suryasekhardatta22@gmail.com>
2022-09-09 20:58:45 +0530

    1

---
+++ Selenium/sel.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import pyautogui as pg
+import time
+
+# driver = webdriver.Chrome()
+
+# driver.get("https://stackoverflow.com")
+
+# element = browser.find_element_by_name("q")
+# element.send_keys("typing")
+
+pg.move(0,1)
