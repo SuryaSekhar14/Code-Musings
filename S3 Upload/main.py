46d72372569ff33aba1985d4703e03bc0dc070aa
SuryaSekhar14 <suryasekhardatta22@gmail.com>
2023-02-19 00:50:21 +0530

    general commit

--- S3 Upload/main.py
+++ S3 Upload/main.py
@@ -1,10 +1,18 @@
 import boto3
 import os
+import dotenv
+
+# Load environment variables
+dotenv.load_dotenv()
 
 # Create an S3 client
-s3 = boto3.client('s3', aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
+try:
+    s3 = boto3.client('s3', aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
                         aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY')
-)
+    )
+    print("S3 client created")
+except Exception as e:
+    print(e)
 
 # Upload a file to the S3 bucket
 file_name = 'example.txt'
